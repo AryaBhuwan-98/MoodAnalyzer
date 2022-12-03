ac155d927868e6ba7e27ba19d784858454344386
AryaBhuwan-98 <aryabhuwan873@gmail.com>
2022-12-03 14:53:59 -0800

    Add:adding program for Throw CustomException Null Or Empty

--- src/main/java/com/bridgelabz/MoodAnalyzer.java
+++ src/main/java/com/bridgelabz/MoodAnalyzer.java
@@ -1,11 +1,24 @@
 package com.bridgelabz;
 
 public class MoodAnalyzer {
-    public String analyseMood(String message) {
+    public String message;
 
-        if (message.contains("sad"))
-            return "SAD";
-        else
-            return "HAPPY";
+    //Parameterized Constructor
+    public MoodAnalyzer(String message) {
+        this.message = message;
+    }
+
+    //Handle Customized Exception using try-catch block
+    public String analyseMood() throws MoodAnalysisException {
+        try {
+            if (message.length() == 0)
+                throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.ENTERED_EMPTY , "Please enter proper message!");
+            if (message.contains("sad"))
+                return "SAD";
+            else
+                return "HAPPY";
+        } catch(NullPointerException e) {
+            throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.ENTERED_NULL , "Please enter proper message!");
+        }
     }
 }
