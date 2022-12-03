eb812a1d5f76bdd7c95427f167f00d4d5e2beaad
AryaBhuwan-98 <aryabhuwan873@gmail.com>
2022-12-03 14:07:50 -0800

    Add:adding program of given message happy or sad by using refactor constructor

---
+++ src/main/java/com/bridgelabz/MoodAnalyzer.java
@@ -0,0 +1,22 @@
+package com.bridgelabz;
+
+public class MoodAnalyzer {
+    private String message;
+
+    //Default Constructor
+    public MoodAnalyzer() {
+    }
+
+    //Parameterized Constructor
+    public MoodAnalyzer(String message) {
+        this.message = message;
+    }
+
+    public String analyseMood() {
+        if (message.contains("sad"))
+            return "SAD";
+        else
+            return "HAPPY";
+    }
+}   
+
